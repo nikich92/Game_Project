d7869a7c7a437fce65c8912e6ee87de354614aa3
nikich92 <nikich92@gmail.com>
2012-10-08 15:50:18 +0300

    collisions and stars

--- Nyan_Cat/js/script.js
+++ Nyan_Cat/js/script.js
@@ -1,8 +1,13 @@
 ﻿// внутренние переменные
 var canvas, ctx;
 var backgroundImage;
-var iBgShiftX = 10;// смещение фона
+var iBgShiftX = 0;// смещение фона
 var cat;
+var st;
+var stars = new Array();
+var star;
+var starX = 600;
+var starY;
 var catW = 170; // ширина кота
 var catH = 80; // высота кота
 var iSprPosition = 0; // инициализация спрайтов
@@ -12,15 +17,50 @@ var bMouseDown = false; // состояние мыши
 var iLastMouseX = 0;
 var iLastMouseY = 0;
 // -------------------------------------------------------------
+// использование Math.round() даст неравномерное распределение!
+function getRandomInt(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // объекты :
-function cat(x, y, w, h, image) {
+//function cat(x, y, w, h, image) {
+//    this.x = x;
+//    this.y = y;
+//    this.w = w;
+//    this.h = h;
+//    this.image = image;
+//    //this.bCat = false;
+//}
+
+function XD(x1,y1,x2,y2){
+
+    return (((x1>x2-obj2.w/2)&&(x1<x2+obj2.w/2))&&((y1>y2obj2.h/2)&&(y1<y2+obj2.h/2)));
+
+}
+
+function collision(obj1,obj2){
+    var XColl=false;
+    var YColl=false;
+
+    var catY = obj1.y + obj1.h;
+    var starY = obj2.y + obj2.h;
+
+    var catX = obj1.x+obj1.w;
+   XColl = ((obj2.y<catY && obj2.y > obj1.y) && (obj2.x<catX && obj2.x>obj1.x)) || ((starY>obj1.y && starY < catY) && (obj2.x<catX && obj2.x>obj1.x));
+   // return XD(obj1.x+obj1.w,obj1.y+obj1.h,obj2.x,obj2.y)&&XD(obj1.x,obj1.y,obj2.x+obj2.w,obj2.y+obj2.h)&&
+     //   XD(obj1.x+obj1.w,obj2.y,obj2.x,obj2.y+obj2.h)&&XD(obj1.x,obj2.y+obj2.h,obj2.x+obj2.w,obj2.y);
+   return XColl;
+   // if(((C1X>C2X-obj2.w/2)&&(C1X<C2X+obj2.w/2))&&((C2Y>C2Y-obj2.h/2)&&(C2Y<C2Y+obj2.h/2))); XColl = true;
+
+}
+function player (x,y,w,h,image)
+{
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.image = image;
-    //this.bCat = false;
 }
 // -------------------------------------------------------------
 
@@ -32,12 +72,11 @@ function clear() { // функция очистки canvas
 function drawScene() { // главная функция отрисовки
     clear(); // очистить canvas
 
-    iBgShiftX -= 2;
-    if (iBgShiftX <= 0) {
-        iBgShiftX = 50;
+    iBgShiftX += 6;
+    if (iBgShiftX >= 480) {
+        iBgShiftX = 0;
     }
-    //ctx.drawImage(backgroundImage, 0 + iBgShiftX, 0, 1000, 940, 0, 0, 1000, 600);
-ctx.drawImage(backgroundImage, iBgShiftX, 0, 400, 360, 0,0,600,360);
+    ctx.drawImage(backgroundImage, iBgShiftX, 0, 960, 900, 0,0,600,360);
     // обновление позиций спрайтов
     iSprPosition++;
     if (iSprPosition >= 11) {
@@ -48,9 +87,15 @@ ctx.drawImage(backgroundImage, iBgShiftX, 0, 400, 360, 0,0,600,360);
     if (bMouseDown) {
         if (iLastMouseX > cat.x) {
             cat.x += 5;
+            if (cat.x>ctx.canvas.width/2)
+            {
+                cat.x=(ctx.canvas.width/2)-1;
+            }
         }
         if (iLastMouseY > cat.y) {
+
             cat.y += 5;
+
         }
         if (iLastMouseX < cat.x) {
             cat.x -= 5;
@@ -58,11 +103,26 @@ ctx.drawImage(backgroundImage, iBgShiftX, 0, 400, 360, 0,0,600,360);
         if (iLastMouseY < cat.y) {
             cat.y -= 5;
         }
+
+    }
+
+    for (var i = 0; i<stars.length;i++)
+    {
+        var shift = getRandomInt(2,5);
+        if (stars[i].x<0-stars[i].w)
+        {
+            stars[i] =new player(getRandomInt(600,1500),getRandomInt(0,360),star.w,star.h,star.image);
+        }
+        if (collision(cat,stars[i]))
+        {
+           stars[i] = new player(getRandomInt(600,1500),getRandomInt(0,360),star.w,star.h,star.image);
+           // continue;
+        }
+        ctx.drawImage(star.image,0,0,star.w,star.h,stars[i].x-=shift,stars[i].y,star.w,star.h);
     }
 
     // отрисовка кота
     ctx.drawImage(cat.image, iSprPosition*cat.w, 0, cat.w, cat.h, cat.x, 180, cat.w, cat.h);
-    //ctx.drawImage(cat.image, 0,0, cat.w, cat.h, 0, 180, cat.w, cat.h);
 }
 
 // -------------------------------------------------------------
@@ -84,14 +144,24 @@ $(function(){
         console.log('Error loading the background image.');
     }
 
-
-
     // инициализация кота
     var ocatImage = new Image();
+    var starImage = new Image();
+    starImage.src = 'images/points_stars.png';
     ocatImage.src = 'images/nyan_cat.png';
     ocatImage.onload = function() {
     }
-    cat = new cat(0, ctx.canvas.height/2, catW, catH, ocatImage);
+
+    star = new player(0,0,51,51,starImage);
+    cat = new player(0, ctx.canvas.height/2, catW, catH, ocatImage);
+    st = getRandomInt(3,8);
+    // starY = getRandomInt(0,360);
+    // starX = 780;
+
+    for (var j = 0;j<st;j++)
+    {
+        stars.push(new player(getRandomInt(600,1500),getRandomInt(0,360),star.w,star.h,star.image))
+    }
 
     $('#scene').mousedown(function(e) { // привязываем событие нажатия мыши(для перетаскивания)
         var mouseX = e.layerX || 0;
@@ -110,31 +180,11 @@ $(function(){
         }
     });
 
-//    $('#scene').mousemove(function(e) { // привязываем событие движения мыши
-//        var mouseX = e.layerX || 0;
-//        var mouseY = e.layerY || 0;
-//        if(e.originalEvent.layerX) {
-//            mouseX = e.originalEvent.layerX;
-//            mouseY = e.originalEvent.layerY;
-//        }
-//
-//        // сохраняем последние координаты
-//        iLastMouseX = mouseX;
-//        iLastMouseY = mouseY;
-//
-//        // перетаскиваем кота
-//        if (cat.bCat) {
-//            cat.x = mouseX;
-//            cat.y = mouseY;
-//        }
-//
-//        }
-//    });
-
     $('#scene').mouseup(function(e) { // привязываем событие отжатия мыши
         cat.bCat = false;
         bMouseDown = false;
     });
 
+    //setTimeout
     setInterval(drawScene, 50); // отрисовка сцены с заданным интервалом
 });
